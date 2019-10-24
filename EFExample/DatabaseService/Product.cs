eb1d1db269207cf825bf0d272063b4dc6c74187b
Cristian <cristianpand@yahoo.com>
2019-10-24 12:35:50 +0200

    add project EFExample

---
+++ EFExample/DatabaseService/Product.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatabaseService
+{
+  public class Product
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+
+  }
+}
