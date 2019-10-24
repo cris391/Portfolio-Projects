121f351be7c6c4da879e7acd737b654709c6ca89
Cristian <cristianpand@yahoo.com>
2019-10-24 13:27:24 +0200

    EFExample add tests

---
+++ EFExample/Tests/Tests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+  public class Tests
+  {
+    [Fact]
+    public void Test1()
+    {
+      var hello = "hei";
+      Assert.Equal("Hello World", "1");
+    }
+  }
+}
