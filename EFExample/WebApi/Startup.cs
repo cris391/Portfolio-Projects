f72285358082adbfe505c7e8f693c70e34845333
Rasmus Lohse <rnlohse@ruc.dk>
2019-10-31 12:03:40 +0100

    my first commit

--- EFExample/WebApi/Startup.cs
+++ EFExample/WebApi/Startup.cs
@@ -22,7 +22,7 @@ namespace WebApi
       services.AddSingleton<IDataService, DataService>();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. DONE
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
